84b15d56712af8c2a649984352c9bf721cfb38e9
alex <alex@alex-acer>
2015-12-06 20:20:17 -0600

    second commit?;

--- src/main/java/edu/wctc/asp/bookwebapp/repository/AuthorRepository.java
+++ src/main/java/edu/wctc/asp/bookwebapp/repository/AuthorRepository.java
@@ -6,10 +6,12 @@ import java.io.Serializable;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.query.Param;
+import org.springframework.data.rest.core.annotation.RepositoryRestResource;
 /**
  *
  * @author alex
  */
+@RepositoryRestResource(collectionResourceRel = "authors", path = "authors")
 public interface AuthorRepository extends JpaRepository<Author, Integer>, Serializable {
     
     @Query("SELECT a FROM Author a JOIN FETCH a.bookCollection WHERE a.authorID = (:id)")
