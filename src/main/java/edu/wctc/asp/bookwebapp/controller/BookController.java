a95ed51f69881cf5c39e12e6b90cb13915533a66
alex <alex@alex-acer>
2015-12-06 21:28:00 -0600

    tracking errors;

--- src/main/java/edu/wctc/asp/bookwebapp/controller/BookController.java
+++ src/main/java/edu/wctc/asp/bookwebapp/controller/BookController.java
@@ -36,7 +36,7 @@ public class BookController extends HttpServlet {
      * @param response servlet response
      * @throws ServletException if a servlet-specific error occurs
      * @throws IOException if an I/O error occurs
-     */
+     */ 
     protected void processRequest(HttpServletRequest request, HttpServletResponse response)
             throws ServletException, IOException {
         response.setContentType("text/html;charset=UTF-8");
